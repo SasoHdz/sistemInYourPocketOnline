7c6a3ba65f862f60a05aa21602cd415645a07d22
= <=>
2022-08-02 19:40:51 -0500

    Retomando proyecto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import { SystemProvider } from './SystemContext';
-import { Home } from './Pages/Home';
 
 
 function App() {
   return (
-    <SystemProvider>
-      <Home />  
-    </SystemProvider>
+    
   );
 }
 
